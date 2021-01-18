4dbf3807943acb0e5a062bc48142b431fa38744a
FatihTolga <75999470+FatihTolga@users.noreply.github.com>
2021-01-18 22:55:07 +0300

    Create 1 (if-Statement).py
    Assignment - 007/1 (if-Statement) Solution

---
+++ Assignment - 007/1 (if-Statement).py
@@ -0,0 +1,6 @@
+name = input("What is your name?: ").strip().title()
+
+if name == "Fatih":
+    print("Hello, Fatih! The password is : W@123")
+elif name != "Fatih":
+    print("Hello " + name + "! See you later.")
